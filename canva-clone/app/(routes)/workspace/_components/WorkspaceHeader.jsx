4d661d7f247191b5654682f8f7473cfb69f856bf
Suchitra Sahoo <suchitrasahoo874@gmail.com>
2025-05-16 16:01:22 +0530

    minor change in logo

--- canva-clone/app/(routes)/workspace/_components/WorkspaceHeader.jsx
+++ canva-clone/app/(routes)/workspace/_components/WorkspaceHeader.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function WorkspaceHeader() {
   return (
     <div className='p-2 px-5 flex justify-between items-center shadow-sm'>
-        <img src={'./Logo.png'} alt='logo' width={100} height={100} className='w-[120px] h-[80px]' />
+        <img src={'/Logo.png'} alt='logo' width={100} height={100} className='w-[120px] h-[80px]' />
     </div>
   )
 }
